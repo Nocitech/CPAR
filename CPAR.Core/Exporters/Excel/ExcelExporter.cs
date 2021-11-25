26a2f5d2b2b4cbc0c1924a6c953e3bddf0c9c6e3
Kristian Hennings <krhe@inventors.dk>
2021-11-25 11:24:14 +0100

    Bugfix to export of excel files

--- CPAR.Core/Exporters/Excel/ExcelExporter.cs
+++ CPAR.Core/Exporters/Excel/ExcelExporter.cs
@@ -42,21 +42,27 @@ namespace CPAR.Core.Exporters.Excel
             var workbook = new XLWorkbook();
             var ws = workbook.Worksheets.Add("Experiment");
             ws.Cell(1, 1).Value = "Name:";
-            ws.Cell(2, 1).Value = "Protocol:";
-            ws.Cell(3, 1).Value = "Path:";
-            ws.Cell(4, 1).Value = "Within Subject Factors:";
-            ws.Cell(5, 1).Value = "Between Subject Factors:";
-            ws.Cell(6, 1).Value = "Subjects:";
-            ws.Cell(7, 1).Value = "Completed subjects:";
-            ws.Column(1).AdjustToContents();
-
             ws.Cell(1, 2).Value = exp.Name;
+
+            ws.Cell(2, 1).Value = "Protocol:";
             ws.Cell(2, 2).Value = exp.Protocol.Name;
+
+            ws.Cell(3, 1).Value = "Path:";
             ws.Cell(3, 2).Value = exp.ExperimentPath;
+
+            ws.Cell(4, 1).Value = "Within Subject Factors:";
             ws.Cell(4, 2).Value = exp.UseWithinSubjectFactors ? SerializeFactors(exp.WithinSubjectFactors) : "none";
+
+            ws.Cell(5, 1).Value = "Between Subject Factors:";
             ws.Cell(5, 2).Value = exp.UseBetweenSubjectFactors ? SerializeFactors(exp.BetweenSubjectFactors) : "none";
+
+            ws.Cell(6, 1).Value = "Subjects:";
             ws.Cell(6, 2).Value = Subject.GetSubjects().Count((s) => true);
+
+            ws.Cell(7, 1).Value = "Completed subjects:";
             ws.Cell(7, 2).Value = Subject.GetSubjects().Count((s) => s.SubjectComplete());
+
+            ws.Column(1).AdjustToContents();
             ws.Column(2).AdjustToContents();
 
             workbook.SaveAs(GetExperimentFile(path), false);
@@ -69,12 +75,12 @@ namespace CPAR.Core.Exporters.Excel
 
             if (factors.Length > 0)
             {
-                AddFactor(builder, factors[0]);
+                AddFactor(builder, 0, factors[0]);
 
                 for (int i = 1; i < factors.Length; ++i)
                 {
                     builder.Append("|");
-                    AddFactor(builder, factors[i]);
+                    AddFactor(builder, i, factors[i]);
                 }
             }
             else
@@ -85,18 +91,18 @@ namespace CPAR.Core.Exporters.Excel
             return builder.ToString();
         }
 
-        private void AddFactor(StringBuilder builder, Factor factor)
+        private void AddFactor(StringBuilder builder, int number, Factor factor)
         {
-            builder.Append(factor.Name);
+            builder.Append($"{number}: {factor.Name}");
 
             if (factor.Levels.Length > 0)
             {
-                builder.Append("(" + factor.Levels[0].Name);
+                builder.Append($"(0: {factor.Levels[0].Name}");
 
                 for (int i = 1; i < factor.Levels.Length; ++i)
                 {
                     builder.Append(", ");
-                    builder.Append(factor.Levels[i].Name);
+                    builder.Append($"{i}: {factor.Levels[i].Name}");
                 }
                 builder.Append(")");
             }
@@ -125,18 +131,24 @@ namespace CPAR.Core.Exporters.Excel
 
         private void ExportSessions(XLWorkbook wb, Subject subject)
         {
+            int index = 1;
+
             foreach (var session in subject.Sessions)
             {
-                Console.WriteLine("   EXPORTING SESSION [ {0} ]", session.ID);
+                Console.WriteLine($"   EXPORTING SESSION [ S{index}:{session.ID} ]");
 
                 foreach (var result in session.Results)
                 {
-                    var ws = wb.Worksheets.Add(session.ID.Length > 0 ?
-                                               String.Format("{0}-{1} {2}", session.ID, result.ID, result.Name) :
-                                               String.Format("{0} {1}", result.ID, result.Name));
+                    var workSheetName = session.ID.Length > 0 ?
+                                        string.Format("{0}-{1}", $"S{index}", result.ID) :
+                                        string.Format("{0}", result.ID);
+
+                    var ws = wb.Worksheets.Add(workSheetName);
 
                     ExportResult(ws, result);
                 }
+
+                ++index;
             }
         }
 
@@ -154,14 +166,17 @@ namespace CPAR.Core.Exporters.Excel
             foreach (var p in result.Data)
             {
                 ws.Cell(index, 1).Value = time;
-                ws.Cell(index, 2).Value = p.stimulating;
-                ws.Cell(index, 3).Value = p.conditioning;
-                ws.Cell(index, 4).Value = p.VAS;
+                ws.Cell(index, 2).Value = Math.Round(p.stimulating * 100)/100.0;
+                ws.Cell(index, 3).Value = Math.Round(p.conditioning * 100)/100;
+                ws.Cell(index, 4).Value = Math.Round(p.VAS * 100)/100;
                 ++index;
                 time += 1.0 / 20.0;
             }
 
+            ws.Column(1).AdjustToContents();
             ws.Column(2).AdjustToContents();
+            ws.Column(3).AdjustToContents();
+            ws.Column(4).AdjustToContents();
         }
 
         #endregion
