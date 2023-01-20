6afea58b022073188820f61aa01cce37e11ee215
Kristian Hennings <krhe@inventors.dk>
2023-01-20 21:13:43 +0100

    Updated ForceStartStimulation

---
+++ CPAR.Communication/AlgometerCompressorMode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPAR.Communication
+{
+    public enum AlgometerCompressorMode
+    {
+        AUTO = 0,
+        MANUAL
+    }
+}
